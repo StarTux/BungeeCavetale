47f03e4ca4d6e3f3f2ac172fbf976660f20be325
StarTux <ths.loy@googlemail.com>
2018-08-03 17:45:35 +0200

    Support bans, PlayerCache, and send login packet

--- src/main/java/com/cavetale/bungee/BungeeCavetale.java
+++ src/main/java/com/cavetale/bungee/BungeeCavetale.java
@@ -8,20 +8,31 @@ import com.winthier.connect.OnlinePlayer;
 import com.winthier.connect.ServerConnection;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import lombok.Value;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
-import net.md_5.bungee.api.connection.ConnectedPlayer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.ServerDisconnectEvent;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Command;
@@ -33,7 +44,9 @@ public final class BungeeCavetale extends Plugin implements ConnectHandler, List
     private final Map<UUID, String> origins = new HashMap<>();
     private Connect connect;
     private LinkedBlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
-    private List<Command> remoteCommands = new ArrayList<>();
+    private final List<Command> remoteCommands = new ArrayList<>();
+    private final List<Command> serverCommands = new ArrayList<>();
+    private Properties bansProperties, playerCacheProperties;
 
     @Override
     public void onEnable() {
@@ -42,20 +55,22 @@ public final class BungeeCavetale extends Plugin implements ConnectHandler, List
                 @Override
                 public void execute(CommandSender sender, String[] args) {
                     if (args.length == 0) return;
-                    switch(args[0]) {
+                    switch (args[0]) {
                     case "reload":
-                        sender.sendMessage("Reloading BungeeCavetale config");
+                        sender.sendMessage(TextComponent.fromLegacyText("Reloading BungeeCavetale config"));
                         loadConfigs();
                         break;
                     case "status":
-                        sender.sendMessage("Clients");
+                        sender.sendMessage(TextComponent.fromLegacyText("Clients"));
                         for (Client client: connect.getClients()) {
-                            sender.sendMessage(client.getName() + " " + client.getStatus());
+                            sender.sendMessage(TextComponent.fromLegacyText(client.getName() + " " + client.getStatus()));
                         }
-                        sender.sendMessage("Servers");
+                        sender.sendMessage(TextComponent.fromLegacyText("Servers"));
                         for (ServerConnection server: connect.getServer().getConnections()) {
-                            sender.sendMessage(server.getName() + " " + server.getStatus());
+                            sender.sendMessage(TextComponent.fromLegacyText(server.getName() + " " + server.getStatus()));
                         }
+                    default:
+                        break;
                     }
                 }
             });
@@ -101,12 +116,22 @@ public final class BungeeCavetale extends Plugin implements ConnectHandler, List
         connect.sendRemoteCommand(new OnlinePlayer(player.getUniqueId(), player.getName()), "daemon", nargs);
     }
 
+    void onServerCommand(CommandSender sender, String label, String[] args) {
+        if (!(sender instanceof ProxiedPlayer)) return;
+        ProxiedPlayer player = (ProxiedPlayer)sender;
+        ServerInfo server = getProxy().getServerInfo(label);
+        if (server == null) return;
+        player.connect(server);
+    }
+
     void loadConfigs() {
         try {
             for (Command command: remoteCommands) getProxy().getPluginManager().unregisterCommand(command);
             remoteCommands.clear();
-            BufferedReader br = new BufferedReader(new FileReader(new File(getDataFolder(), "remote_commands.txt")));
+            BufferedReader br;
             String line;
+            // Remote commands
+            br = new BufferedReader(new FileReader(new File(getDataFolder(), "remote_commands.txt")));
             while (null != (line = br.readLine())) {
                 if (line.startsWith("#")) continue;
                 final String label = line;
@@ -116,9 +141,45 @@ public final class BungeeCavetale extends Plugin implements ConnectHandler, List
                         }
                     });
             }
-            for (Command command: remoteCommands) getProxy().getPluginManager().registerCommand(this, command);
+            for (Command command: remoteCommands) {
+                System.out.println("[Cavetale] Registering remote command \"" + command.getName() + "\"...");
+                getProxy().getPluginManager().registerCommand(this, command);
+            }
+            // Server commands
+            serverCommands.clear();
+            for (Command command: serverCommands) getProxy().getPluginManager().unregisterCommand(command);
+            br = new BufferedReader(new FileReader(new File(getDataFolder(), "server_commands.txt")));
+            while (null != (line = br.readLine())) {
+                if (line.startsWith("#")) continue;
+                final String label = line;
+                serverCommands.add(new Command(label, "", new String[0]) {
+                        @Override public void execute(CommandSender sender, String[] args) {
+                            onServerCommand(sender, label, args);
+                        }
+                    });
+            }
+            for (Command command: serverCommands) {
+                System.out.println("[Cavetale] Registering server command \"" + command.getName() + "\"...");
+                getProxy().getPluginManager().registerCommand(this, command);
+            }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        bansProperties = new Properties();
+        try {
+            bansProperties.load(new FileInputStream(new File(getDataFolder(), "bans.properties")));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (IllegalArgumentException iae) {
+            iae.printStackTrace();
+        }
+        playerCacheProperties = new Properties();
+        try {
+            playerCacheProperties.load(new FileInputStream(new File(getDataFolder(), "playercache.properties")));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (IllegalArgumentException iae) {
+            iae.printStackTrace();
         }
     }
 
@@ -164,6 +225,126 @@ public final class BungeeCavetale extends Plugin implements ConnectHandler, List
         if (cancelServer == null) return;
         event.setCancelled(true);
         event.setCancelServer(cancelServer);
+    }
+
+    @Value
+    static final class Ban {
+        String type;
+        String admin;
+        String reason;
+    }
+
+    @EventHandler
+    public void onLogin(LoginEvent event) {
+        UUID uuid = event.getConnection().getUniqueId();
+        String name = event.getConnection().getName();
+        Ban ban = getActiveBan(uuid);
+        if (ban != null) {
+            String admin = ban.admin;
+            if (admin != null) {
+                UUID adminId;
+                try {
+                    adminId = UUID.fromString(admin);
+                    PlayerCache cache = findPlayerCache(adminId, null);
+                    admin = cache.name;
+                } catch (IllegalArgumentException iae) {
+                    iae.printStackTrace();
+                    admin = null;
+                }
+            }
+            String reason = ban.reason;
+            event.setCancelled(true);
+            ComponentBuilder builder = new ComponentBuilder("You are banned!").color(ChatColor.RED);
+            if (admin != null) {
+                builder.append("\nBy: ").color(ChatColor.WHITE)
+                    .append(admin).color(ChatColor.GRAY);
+            }
+            if (reason != null) {
+                builder.append("\nReason: ").color(ChatColor.WHITE)
+                    .append(reason).color(ChatColor.GRAY);
+            }
+            builder.append("\nPlease appeal your ban at ").color(ChatColor.WHITE);
+            builder.append("https://cavetale.com").color(ChatColor.BLUE);
+            event.setCancelReason(builder.create());
+            return;
+        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("uuid", uuid);
+        map.put("name", name);
+        connect.broadcast("BUNGEE_PLAYER_LOGIN", map);
+    }
+
+    Connection connectionFromProperties(Properties properties) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+            return null;
+        }
+        String host = properties.getProperty("host", "localhost");
+        String port = properties.getProperty("port", "3306");
+        String database = properties.getProperty("database", "Bans");
+        String user = properties.getProperty("user", "user");
+        String password = properties.getProperty("password", "password");
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            return null;
+        }
+        return connection;
+    }
+
+    Ban getActiveBan(UUID player) {
+        Connection connection = connectionFromProperties(bansProperties);
+        if (connection == null) return null;
+        String table = bansProperties.getProperty("table", "bans");
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM `" + table + "` WHERE `player` = '" + player.toString() + "' AND `type` = 'ban' AND (`expiry` IS NULL OR `expiry` < NOW())");
+            if (result.next()) {
+                return new Ban(result.getString("type"), result.getString("admin"), result.getString("reason"));
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return null;
+    }
+
+    @Value
+    static final class PlayerCache {
+        private UUID uuid;
+        private String name;
+    }
+
+    PlayerCache findPlayerCache(UUID uuid, String name) {
+        Connection connection = connectionFromProperties(playerCacheProperties);
+        if (connection == null) return null;
+        String table = playerCacheProperties.getProperty("table", "bans");
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result;
+            if (uuid != null) {
+                result = statement.executeQuery("SELECT * FROM `" + table + "` WHERE `uuid` = '" + uuid.toString() + "'");
+            } else {
+                result = statement.executeQuery("SELECT * FROM `" + table + "` WHERE `name` = '" + name + "'");
+            }
+            if (result.next()) {
+                try {
+                    uuid = UUID.fromString(result.getString("uuid"));
+                } catch (IllegalArgumentException iae) {
+                    iae.printStackTrace();
+                    return null;
+                }
+                name = result.getString("name");
+                return new PlayerCache(uuid, name);
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return null;
     }
 
     // Connect Handler
